94963bd6625a9a1a6fdfc171ff09755f96acf111
Raphael Ramos <raphael.opensource@gmail.com>
2025-09-04 06:37:49 -0300

    docs: explain where secrets should be created

--- services/apis_monorepo_triggers/main.tf
+++ services/apis_monorepo_triggers/main.tf
@@ -9,15 +9,18 @@ resource "google_cloudbuildv2_connection" "connection" {
   location = var.region
   project  = var.project_id
   gitlab_config {
-    host_uri                      = "gitlab.com"
+    host_uri = "gitlab.com"
     # SecretManager resource containing the webhook secret of a GitLab Enterprise project, formatted as 'projects//secrets//versions/*'
+    # (created on Gitlab > Settings > Access Tokens)
     webhook_secret_secret_version = ""
     read_authorizer_credential {
       # A SecretManager resource containing the user token that authorizes the Cloud Build connection. Format: 'projects/*/secrets/*/versions/*'.
+      # (created on Gitlab > Settings > Access Tokens)
       user_token_secret_version = ""
     }
     authorizer_credential {
       # A GitLab personal access token with the 'api' scope access
+      # (created on Gitlab > Settings > Webhooks)
       user_token_secret_version = ""
     }
   }
@@ -30,7 +33,7 @@ resource "google_cloudbuild_trigger" "api_triggers" {
   project         = var.project_id
   location        = var.region
   service_account = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
-  included_files = each.value.included_path
+  included_files  = each.value.included_path
 
   repository_event_config {
     repository = "projects/${var.project_id}/locations/${var.region}/connections/geowellex/repositories/raphaelgeowellex-apis-monorepo"
@@ -38,7 +41,7 @@ resource "google_cloudbuild_trigger" "api_triggers" {
       branch = each.value.branch
     }
   }
-  
+
   substitutions = merge(local.substitutions, each.value.substitutions)
-  filename = each.value.filename
+  filename      = each.value.filename
 }
