6685760eef5f1f4e8ddfa0e7f74c4d981bf64701
Raphael Ramos <raphael.opensource@gmail.com>
2025-09-04 06:01:50 -0300

    style: add comment inside resource definition

--- services/streamlit_app/main.tf
+++ services/streamlit_app/main.tf
@@ -4,20 +4,21 @@ provider "google-beta" {
   zone    = var.zone
 }
 
-# Cloud Build Trigger
-#
-# Automatically builds and deploys the Docker image to Cloud Run 
-# whenever there is a new push to the `main` branch of the specified GitHub repository.
-#
-# The build configuration is defined in the `streamlit/cloudbuild.yaml` file.
-# Substitutions provide contextual variables to be used within the build process.
-#
-# Important:
-# - The GitHub repository must be connected manually via the GCP Console:
-#   Cloud Build → Triggers → Connect Repository
-# - Manual approval is not required before each build is executed.
-
 resource "google_cloudbuild_trigger" "cloudbuild_trigger" {
+  
+  # Cloud Build Trigger
+  #
+  # Automatically builds and deploys the Docker image to Cloud Run 
+  # whenever there is a new push to the `main` branch of the specified GitHub repository.
+  #
+  # The build configuration is defined in the `streamlit/cloudbuild.yaml` file.
+  # Substitutions provide contextual variables to be used within the build process.
+  #
+  # Important:
+  # - The GitHub repository must be connected manually via the GCP Console:
+  #   Cloud Build → Triggers → Connect Repository
+  # - Manual approval is not required before each build is executed.
+
   name            = "${var.service_name}-ci-trigger"
   project         = var.project_id
   location        = var.region
